bf4d736aec675113f3a4f70382d057c63c37d81d
Fabio Ticconi <fabio.ticconi@gmail.com>
2015-06-21 14:32:05 +0200

    some indels results and others

--- data_analysis/meta_bsub.sh
+++ data_analysis/meta_bsub.sh
@@ -1,29 +1,32 @@
 #!/usr/bin/env bash
 
-if [[ $# < 1 ]]
+if (( $# < 4 ))
 then
-    echo "`basename $0` [cat/bsub] command args"
+    echo "`basename $0` [cat/bsub] name W command args"
     exit -1
 fi
 
-command -v $2 >/dev/null 2>&1 || { echo >&2 "Command '$1' not available"; exit -1; }
+command -v $4 >/dev/null 2>&1 || { echo >&2 "Command '$1' not available"; exit -1; }
 
-bin=`which $2`
-args="${*:3}"
+bin=`which $4`
+args="${*:5}"
+
+name="$2"
+W="$3"
 
 $1 <<EOF
 #!/usr/bin/env zsh
 ### Job name
-#BSUB -J $2
+#BSUB -J $name
  
 ### File / path where STDOUT & STDERR will be written
 ### %J is the job ID, %I is the array ID
-#BSUB -o $2.log.%J.%I
+#BSUB -o ${name}.log.%J.%I
  
 ### Request the time you need for execution in minutes
 ### The format for the parameter is: [hour:]minute,
 ### that means for 80 minutes you could also use this: 1:20
-#BSUB -W 3:0
+#BSUB -W $W
  
 ### Request memory you need for your job in TOTAL in MB
 #BSUB -M 15000
