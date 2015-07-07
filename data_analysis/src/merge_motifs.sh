d68b68942ee89c1bfd39ab390a16d31c10227277
Fabio Ticconi <fabio.ticconi@gmail.com>
2015-07-07 16:57:31 +0200

    generated new 'merged' motif files, containing all runs, experiments and consensus for a specific organism/platform

--- data_analysis/src/merge_motifs.sh
+++ data_analysis/src/merge_motifs.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Goes inside every organism/platform combination,
+# then it takes all available .data files (in all three possible levels:
+# consensus, experiment and run) and merges them together.
+# this means that all the contingency tables will be merged and nothing
+# will be lost
+
 if [ ! -d "./organisms" ]
 then
 	echo "You must cd into the data_analysis subdir, ie you must see the 'organisms' directory"
@@ -67,10 +73,11 @@ do
 				n=2
 				for q in 4 8 10
 				do
-					simple_motif_merger.py $q $n {$restype,*/$restype,*/*/$restype}/results_${q}-grams_${n}n.data -d $d $opts > ${platform}_${type}_${restype}_${q}-grams_${n}n_d${d2}.data
+					files=`shopt -s nullglob; echo {$restype*,*/$restype,*/*/$restype}/results_${q}-grams_${n}n.data`
+					simple_motif_merger.py $q $n $files -d $d $opts > ${platform}_${type}_${restype}_${q}-grams_${n}n_d${d2}.data
 				done
-				
-				simple_motif_merger.py 8 4 {$restype,*/$restype,*/*/$restype}/results_8-grams_4n.data -d $d $opts > ${platform}_${type}_${restype}_8-grams_4n_d${d2}.data
+				files=`shopt -s nullglob; echo {$restype*,*/$restype,*/*/$restype}/results_8-grams_4n.data`
+				simple_motif_merger.py 8 4 $files -d $d $opts > ${platform}_${type}_${restype}_8-grams_4n_d${d2}.data
 			done
 		done
 
